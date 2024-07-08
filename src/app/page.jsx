4e39873babbcc4db2f7d3dd2920b5031e65897f5
pr-citrate <pr.citrate@gmail.com>
2024-07-08 17:00:47 +0900

    Update: Add structures of form inputs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,23 @@ import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Card } from "@/components/ui/card";
 import { Form } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Popover,
   PopoverTrigger,
   PopoverContent,
 } from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { CalendarIcon } from "lucide-react";
 
 export default function Home() {
   return (
@@ -16,9 +27,33 @@ export default function Home() {
         <Form>
           <Label>Lorem ipsum</Label>
 
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Select time" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Time</SelectLabel>
+                <SelectItem value="1">time 1</SelectItem>
+                <SelectItem value="2">time 2</SelectItem>
+                <SelectItem value="3">time 3</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+
+          <Input placeholder="reason" type="text" />
+          <Input placeholder="tel" type="tel" />
+
+          <div className="flex flex-row">
+            <Input placeholder="name" type="text" />
+            <Input placeholder="number" type="text" />
+          </div>
+
           <Popover>
             <PopoverTrigger asChild>
-              <Button variant={"outline"}>date</Button>
+              <Button variant="outline">
+                <CalendarIcon />
+              </Button>
             </PopoverTrigger>
             <PopoverContent>
               <Calendar mode="single" />
