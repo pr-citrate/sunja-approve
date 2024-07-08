c37cd85853e4be91b03fa7a55d177a0fde64ddb4
pr-citrate <pr.citrate@gmail.com>
2024-07-09 08:29:05 +0900

    Refactor: Convert for loop into map

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState } from 'react';
+import { useId, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Form } from "@/components/ui/form";
@@ -17,51 +17,8 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-const Home = () => {
-  const numParticipantsRef = useRef(2); 
-  const [update, setUpdate] = useState(false); 
-
-  const handleNumParticipantsChange = (value) => {
-    const numParticipants = parseInt(value);
-    if (!isNaN(numParticipants)) {
-      if (numParticipants === 2 || numParticipants === 3 || numParticipants === 4 || numParticipants === 5) {
-        numParticipantsRef.current = numParticipants;
-        setUpdate(!update); 
-      }
-    }
-  };
-
-  const renderStudentInputs = () => {
-    let inputs = [];
-    inputs.push(
-      <div className="flex flex-row mb-4" key="representative">
-        <div className="mr-2 flex-1">
-          <Label htmlFor="representativeName" className="block mb-1">대표자 이름</Label>
-          <Input id="representativeName" placeholder="대표자 이름" type="text" className="text-lg" />
-        </div>
-        <div className="flex-1">
-          <Label htmlFor="representativeId" className="block mb-1">대표자 학번</Label>
-          <Input id="representativeId" placeholder="대표자 학번" type="text" className="text-lg" />
-        </div>
-      </div>
-    );
-
-    for (let i = 1; i < numParticipantsRef.current; i++) {
-      inputs.push(
-        <div className="flex flex-row mb-4" key={i}>
-          <div className="mr-2 flex-1">
-            <Label htmlFor={`name${i}`} className="block mb-1">{`이름 ${i}`}</Label>
-            <Input id={`name${i}`} placeholder={`이름 ${i}`} type="text" className="text-lg" />
-          </div>
-          <div className="flex-1">
-            <Label htmlFor={`id${i}`} className="block mb-1">{`학번 ${i}`}</Label>
-            <Input id={`id${i}`} placeholder={`학번 ${i}`} type="text" className="text-lg" />
-          </div>
-        </div>
-      );
-    }
-    return inputs;
-  };
+export default function Home() {
+  const [numParticipants, setNumParticipants] = useState(3);
 
   return (
     <main className="grid justify-items-center items-center w-full h-full">
@@ -69,7 +26,9 @@ const Home = () => {
         <Form>
           <Label className="text-xl mb-4">순자증 신청</Label>
           <div className="mb-4 w-full">
-            <Label htmlFor="time" className="block mb-1">사용 시간</Label>
+            <Label htmlFor="time" className="block mb-1">
+              사용 시간
+            </Label>
             <Select id="time" className="text-lg w-full">
               <SelectTrigger>
                 <SelectValue placeholder="사용 시간" />
@@ -86,8 +45,14 @@ const Home = () => {
           </div>
 
           <div className="mb-4 w-full">
-            <Label htmlFor="participants" className="block mb-1">사용 인원</Label>
-            <Select id="participants" className="text-lg w-full" onValueChange={(value) => handleNumParticipantsChange(value)}>
+            <Label htmlFor="participants" className="block mb-1">
+              사용 인원
+            </Label>
+            <Select
+              id="participants"
+              className="text-lg w-full"
+              onValueChange={(value) => setNumParticipants(value)}
+            >
               <SelectTrigger className="outline-none">
                 <SelectValue placeholder="사용 인원" />
               </SelectTrigger>
@@ -104,21 +69,87 @@ const Home = () => {
           </div>
 
           <div className="mb-4 w-full">
-            <Label htmlFor="reason" className="block mb-1">사유</Label>
-            <Input id="reason" placeholder="사유" type="text" className="text-lg w-full" />
+            <Label htmlFor="reason" className="block mb-1">
+              사유
+            </Label>
+            <Input
+              id="reason"
+              placeholder="사유"
+              type="text"
+              className="text-lg w-full"
+            />
           </div>
           <div className="mb-4 w-full">
-            <Label htmlFor="phone" className="block mb-1">대표자 전화번호</Label>
-            <Input id="phone" placeholder="대표자 전화번호" type="tel" className="text-lg w-full" />
+            <Label htmlFor="phone" className="block mb-1">
+              대표자 전화번호
+            </Label>
+            <Input
+              id="phone"
+              placeholder="대표자 전화번호"
+              type="tel"
+              className="text-lg w-full"
+            />
           </div>
 
-          {renderStudentInputs()}
+          <div className="flex flex-row mb-4" key="representative">
+            <div className="mr-2 flex-1">
+              <Label htmlFor="representativeName" className="block mb-1">
+                대표자 이름
+              </Label>
+              <Input
+                id="representativeName"
+                placeholder="대표자 이름"
+                type="text"
+                className="text-lg"
+              />
+            </div>
+            <div className="flex-1">
+              <Label htmlFor="representativeId" className="block mb-1">
+                대표자 학번
+              </Label>
+              <Input
+                id="representativeId"
+                placeholder="대표자 학번"
+                type="text"
+                className="text-lg"
+              />
+            </div>
+          </div>
 
-          <Button type="submit" className="text-lg mt-4">Submit</Button>
+          {[...Array(numParticipants - 1)].map((_, i) => {
+            return (
+              <div className="flex flex-row mb-4" key={i + 2}>
+                <div className="mr-2 flex-1">
+                  <Label htmlFor={`name${i + 2}`} className="block mb-1">
+                    {`이름 ${i + 2}`}
+                  </Label>
+                  <Input
+                    id={`name${i + 2}`}
+                    placeholder={`이름 ${i + 2}`}
+                    type="text"
+                    className="text-lg"
+                  />
+                </div>
+                <div className="flex-1">
+                  <Label htmlFor={`id${i + 2}`} className="block mb-1">{`학번 ${
+                    i + 2
+                  }`}</Label>
+                  <Input
+                    id={`id${i + 2}`}
+                    placeholder={`학번 ${i + 2}`}
+                    type="text"
+                    className="text-lg"
+                  />
+                </div>
+              </div>
+            );
+          })}
+
+          <Button type="submit" className="text-lg mt-4">
+            Submit
+          </Button>
         </Form>
       </Card>
     </main>
   );
-};
-
-export default Home;
+}
