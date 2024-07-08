36735541cbd18328c458b0272ba8d18436581f90
pr-citrate <pr.citrate@gmail.com>
2024-07-08 11:13:55 +0900

    Update: Add basic form layout only with calendar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Card } from "@/components/ui/card";
+import { Form } from "@/components/ui/form";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="grid justify-items-center items-center w-full h-full">
+      <Card className="min-w-80 grid justify-items-center items-center min-h-80">
+        <Form>
+          <Label>Lorem ipsum</Label>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant={"outline"}>date</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <Calendar mode="single" />
+            </PopoverContent>
+          </Popover>
+
+          <Button type="submit">Submit</Button>
+        </Form>
+      </Card>
+    </main>
+  );
 }
