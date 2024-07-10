669675bb642d2d0671c57e269717e56b871d1975
pr-citrate <pr.citrate@gmail.com>
2024-07-10 17:13:58 +0900

    Update: Remove useless things

--- src/app/pages/success.jsx
+++
@@ -1,7 +0,0 @@
-export default function Success() {
-  return (
-    <div className="grid justify-items-center items-center w-full h-full">
-      <h1 className="text-2xl">신청이 성공적으로 제출되었습니다!</h1>
-    </div>
-  );
-}
