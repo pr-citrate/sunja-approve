05a268884d47e4b9bf2a50613e8ec73313bacbd6
Acetylcholine <jbs34_1211@js.hs.kr>
2024-07-16 18:04:11 +0900

    feat: Add Label component to page.jsx

--- src/app/present/page.jsx
+++ src/app/present/page.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { useForm, FormProvider } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
 import {
   Table,
   TableBody,
