9c4f56fa98906785c81276a1ce5a114fb3b96af7
Acetylcholine <jbs34_1211@js.hs.kr>
2024-07-17 22:43:31 +0900

    Delete many things

--- src/app/adminstart/page.jsx
+++ src/app/adminstart/page.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 
-export default function HomePage() {
+export default function Home() {
   const handleButtonClick = (url) => {
     if (typeof window !== "undefined") {
       window.location.href = url;
@@ -18,13 +18,13 @@ export default function HomePage() {
         <Label className="text-xl mb-4">선택하세요</Label>
         <Button
           className="text-lg mb-4 w-full"
-          onClick={() => handleButtonClick("/admin")}
+          onClick={handleButtonClick("/admin")}
         >
           신청 승인
         </Button>
         <Button
           className="text-lg mb-4 w-full"
-          onClick={() => handleButtonClick("/present")}
+          onClick={handleButtonClick("/status")}
         >
           신청 현황
         </Button>
