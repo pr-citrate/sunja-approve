5507eea1ac80525edcf8befe4322eaf77a7b1c6a
Acetylcholine <jbs34_1211@js.hs.kr>
2024-07-17 17:59:22 +0900

    Refactor: Update Home component to display request status as "승인" or "거부" instead of "미승인"

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,286 +1,251 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { useForm, FormProvider } from "react-hook-form";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  useReactTable,
-  getCoreRowModel,
-  flexRender,
-} from "@tanstack/react-table";
-
-const columns = (data, setData) => [
-  {
-    accessorKey: "name",
-    header: "신청자 이름",
-  },
-  {
-    accessorKey: "contact",
-    header: "전화번호",
-  },
-  {
-    accessorKey: "count",
-    header: "총인원",
-  },
-  {
-    accessorKey: "time",
-    header: "신청 교시",
-  },
-  {
-    accessorKey: "reason",
-    header: "사유",
-  },
-  {
-    accessorKey: "status",
-    header: "상태",
-  },
-  {
-    id: "details",
-    header: "총 신청자",
-    cell: ({ row }) => (
-      <Button
-        onClick={() => {
-          alert(
-            row.original.applicant
-              .map((applicant) => `${applicant.name} (${applicant.number})`)
-              .join("\n")
-          );
-        }}
-      >
-        더보기
-      </Button>
-    ),
-  },
-  {
-    id: "approve",
-    header: "확인",
-    cell: ({ row }) => (
-      <Button
-        onClick={async () => {
-          try {
-            const isApproved = row.original.status === "승인";
-            const newStatus = isApproved ? "미승인" : "승인";
-
-            const response = await fetch(
-              `/api/requests?id=${row.original.id}`,
-              {
-                method: "PATCH",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  status: newStatus,
-                }),
-              }
-            );
-
-            if (!response.ok) {
-              throw new Error("Status update failed");
-            }
-
-            const updatedData = data.map((d) =>
-              d.id === row.original.id ? { ...d, status: newStatus } : d
-            );
-            setData(updatedData);
-          } catch (error) {
-            console.error("Error updating status:", error);
-          }
-        }}
-      >
-        {row.original.status === "승인" ? "승인 취소" : "승인"}
-      </Button>
-    ),
-  },
-  {
-    id: "reject",
-    header: "거부",
-    cell: ({ row }) => (
-      <Button
-        onClick={async () => {
-          try {
-            const response = await fetch(
-              `/api/requests?id=${row.original.id}`,
-              {
-                method: "DELETE",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              }
-            );
-
-            if (!response.ok) {
-              throw new Error("Request deletion failed");
-            }
-
-            const updatedData = data.filter((d) => d.id !== row.original.id);
-            setData(updatedData);
-            alert("거부 되었습니다.");
-          } catch (error) {
-            console.error("Error deleting request:", error);
-          }
-        }}
-      >
-        거부
-      </Button>
-    ),
-  },
-];
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useForm } from "react-hook-form";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
-  const [password, setPassword] = useState("");
-  const [isPasswordCorrect, setIsPasswordCorrect] = useState(false);
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const methods = useForm();
+  const [numApplicant, setNumApplicant] = useState(2);
+  const form = useForm();
 
-  const table = useReactTable({
-    data,
-    columns: columns(data, setData),
-    getCoreRowModel: getCoreRowModel(),
-  });
-
-  const handlePasswordSubmit = (e) => {
-    e.preventDefault();
-    if (password === "000000") {
-      setIsPasswordCorrect(true);
-    } else {
-      alert("비밀번호가 틀렸습니다. 다시 시도해주세요.");
-    }
+  const handleButtonClick = (url) => {
+    window.location.href = url;
   };
+  const onSubmit = async (data) => {
+    console.log("Submitting data:", data); // 폼 데이터를 콘솔에 출력
 
-  const fetchData = async () => {
-    setIsLoading(true);
     try {
-      const response = await fetch("/api/requests");
+      const response = await fetch("/api/requests", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
       const result = await response.json();
-      console.log("클라이언트에서 받아온 데이터:", result.requests);
-
-      const transformedData = result.requests.map((request) => ({
-        ...request,
-        name: request.applicant[0]?.name || "N/A",
-        count: `${request.applicant.length}명`,
-        time: `${request.time}교시`,
-        status:
-          request.isApproved === null
-            ? "미승인"
-            : request.isApproved
-            ? "승인"
-            : "거부",
-      }));
-
-      console.log("변환된 데이터:", transformedData);
-      setData(transformedData);
+      console.log("Data submitted successfully:", result);
+      alert("제출되었습니다.");
+      window.location.reload(); // 제출 성공 시 페이지를 초기화
     } catch (error) {
-      console.error("데이터 가져오기 오류:", error);
-      setData([]);
-    } finally {
-      setIsLoading(false);
+      console.error("Error submitting data:", error);
+      alert("제출 실패");
     }
   };
-
-  useEffect(() => {
-    if (isPasswordCorrect) {
-      fetchData();
-    }
-  }, [isPasswordCorrect]);
 
   return (
-    <FormProvider {...methods}>
-      <main className="flex justify-center items-center w-full h-screen">
-        {!isPasswordCorrect ? (
-          <Card className="w-96 grid justify-items-center items-center p-8">
-            <form
-              onSubmit={handlePasswordSubmit}
-              className="w-full grid justify-items-center"
-            >
-              <Label htmlFor="password" className="text-xl mb-4">
-                비밀번호 입력
-              </Label>
-              <Input
-                id="password"
-                type="password"
-                placeholder="비밀번호를 입력하세요"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="mb-4 text-lg w-full"
-              />
-              <Button type="submit" className="text-lg mt-4 w-full">
-                로그인
-              </Button>
-              <Button
-                type="button"
-                onClick={() => (window.location.href = "/standard")}
-                className="text-lg mt-4"
-              >
-                뒤로
-              </Button>
-            </form>
-          </Card>
-        ) : (
-          <Card className="min-w-screen grid justify-items-center items-center p-8 m-12 min-h-screen">
-            <div className="rounded-md border mb-4 w-full">
-              {isLoading ? (
-                <p>로딩 중...</p>
-              ) : !data || data.length === 0 ? (
-                <p>신청 목록이 없습니다</p>
-              ) : (
-                <Table className="w-full">
-                  <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                      <TableRow key={headerGroup.id}>
-                        {headerGroup.headers.map((header) => (
-                          <TableHead key={header.id}>
-                            {header.isPlaceholder
-                              ? null
-                              : flexRender(
-                                  header.column.columnDef.header,
-                                  header.getContext()
-                                )}
-                          </TableHead>
-                        ))}
-                      </TableRow>
-                    ))}
-                  </TableHeader>
-                  <TableBody>
-                    {table.getRowModel().rows.map((row) => (
-                      <TableRow key={row.id}>
-                        {row.getVisibleCells().map((cell) => (
-                          <TableCell key={cell.id}>
-                            {flexRender(
-                              cell.column.columnDef.cell,
-                              cell.getContext()
-                            )}
-                          </TableCell>
-                        ))}
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
+    <main className="grid justify-items-center items-center w-full min-h-full">
+      <Card className="min-w-screen grid justify-items-center items-center p-8 m-12 min-h-96">
+        <Form {...form}>
+          <form
+            className="w-full h-full flex flex-col items-center justify-center"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <Label className="text-xl mb-4">순자증 신청</Label>
+            <FormField
+              control={form.control}
+              name="time"
+              rules={{ required: "사용 시간을 선택하세요" }}
+              render={({ field }) => (
+                <FormItem className="mb-4 w-full">
+                  <FormLabel htmlFor="time" className="block mb-1">
+                    사용 시간
+                  </FormLabel>
+                  <FormControl>
+                    <Select
+                      id="time"
+                      className="text-lg w-full"
+                      onValueChange={field.onChange}
+                      value={field.value}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="사용 시간" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>시간</SelectLabel>
+                          <SelectItem value="1">야자 1교시</SelectItem>
+                          <SelectItem value="2">야자 2교시</SelectItem>
+                          <SelectItem value="3">야자 3교시</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                </FormItem>
               )}
-            </div>
-            <div className="flex space-x-4 mt-4">
-              <Button
-                type="button"
-                onClick={() => window.location.reload()}
-                className="text-lg"
-              >
-                뒤로
-              </Button>{" "}
-            </div>
-          </Card>
-        )}
-      </main>
-    </FormProvider>
+            />
+            <FormField
+              control={form.control}
+              name="applicant"
+              rules={{ required: "사용 인원을 선택하세요" }}
+              render={({ field }) => (
+                <FormItem className="mb-4 w-full">
+                  <FormLabel htmlFor="applicant" className="block mb-1">
+                    사용 인원
+                  </FormLabel>
+                  <FormControl>
+                    <Select
+                      id="applicant"
+                      className="text-lg w-full"
+                      onValueChange={(value) => {
+                        setNumApplicant(parseInt(value));
+                        field.onChange(value);
+                      }}
+                      value={field.value}
+                    >
+                      <SelectTrigger className="outline-none">
+                        <SelectValue placeholder="사용 인원" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>인원</SelectLabel>
+                          <SelectItem value="2">2명</SelectItem>
+                          <SelectItem value="3">3명</SelectItem>
+                          <SelectItem value="4">4명</SelectItem>
+                          <SelectItem value="5">5명</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="reason"
+              rules={{ required: "사유를 입력하세요" }}
+              render={({ field }) => (
+                <FormItem className="mb-4 w-full">
+                  <FormLabel htmlFor="reason" className="block mb-1">
+                    사유
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      id="reason"
+                      placeholder="사유"
+                      type="text"
+                      className="text-g w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="contact"
+              rules={{ required: "대표자 전화번호를 입력하세요" }}
+              render={({ field }) => (
+                <FormItem className="mb-4 w-full">
+                  <Label htmlFor="contact" className="block mb-1">
+                    전화번호 (대표자)
+                  </Label>
+                  <FormControl>
+                    <Input
+                      id="contact"
+                      placeholder="전화번호 (대표자)"
+                      type="tel"
+                      className="text-g w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <AnimatePresence>
+              {[...Array(numApplicant)].map((_, i) => {
+                const number = `${i + 1}${i ? "" : " (대표자)"}`;
+                return (
+                  <motion.div
+                    className="flex flex-row mb-4 space-y-0 space-x-2"
+                    key={i}
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 20 }}
+                  >
+                    <FormField
+                      control={form.control}
+                      name={`applicant[${i}].name`}
+                      rules={{ required: "이름을 입력하세요" }}
+                      render={({ field }) => (
+                        <FormItem className="flex-1">
+                          <Label htmlFor={`name${i}`} className="block mb-1">
+                            {`이름 ${number}`}
+                          </Label>
+                          <FormControl>
+                            <Input
+                              id={`name${i}`}
+                              placeholder={`이름 ${number}`}
+                              type="text"
+                              className="text-g"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name={`applicant[${i}].number`}
+                      rules={{ required: "학번을 입력하세요" }}
+                      render={({ field }) => (
+                        <FormItem className="flex-1">
+                          <Label htmlFor={`id${i}`} className="block mb-1">
+                            {`학번 ${number}`}
+                          </Label>
+                          <FormControl>
+                            <Input
+                              id={`id${i}`}
+                              placeholder={`학번 ${number}`}
+                              type="text"
+                              className="text-g"
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </motion.div>
+                );
+              })}
+            </AnimatePresence>
+            <Button type="submit" className="text-lg mt-4">
+              제출
+            </Button>
+            <Button
+              type="button"
+              onClick={() => handleButtonClick("/present")}
+              className="text-lg mt-4"
+            >
+              신청 현황
+            </Button>
+          </form>
+        </Form>
+      </Card>
+    </main>
   );
 }
