876d645792df3ea4f316dbff2a7fc1e7f56bdb51
pr-citrate <pr.citrate@gmail.com>
2024-07-21 14:25:55 +0900

    Update: Update validation rules

--- src/schema.js
+++ src/schema.js
@@ -1,15 +1,15 @@
 import { z } from "zod";
 
 const applicantSchema = z.object({
-  name: z.string().min(1, "이름을 입력하세요"),
-  number: z.string().min(1, "학번을 입력하세요"),
+  name: z.string().min(2, ""),
+  number: z.string().regex(/^[1-4][1-3](0[1-9]|[1-2][0-9]|30)$/, "학번을 입력하세요"),
 });
 
 const formSchema = z.object({
-  time: z.string().min(1, "사용 시간을 선택하세요"),
-  applicant: z.array(applicantSchema).min(3, "사용 인원을 선택하세요"),
+  time: z.enum(["1", "2", "3"], { message:"사용 시간을 선택하세요" }),
+  applicant: z.array(applicantSchema).min(2, "사용 인원을 선택하세요"),
   reason: z.string().min(1, "사유를 입력하세요"),
-  contact: z.string().min(1, "대표자 전화번호를 입력하세요"),
+  contact: z.string().regex(/^(010-\d{4}-\d{4}|010\d{8})$/  , "대표자 전화번호를 입력하세요"),
 });
 
 export default formSchema;
