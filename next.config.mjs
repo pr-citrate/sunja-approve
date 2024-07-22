437d1c435565aa86660035da94e0673bf3473495
Acetylcholine <ach080306@naver.com>
2024-07-23 08:12:27 +0900

    chore: Update environment variable names for Xata API key and branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      NEXT_PUBLIC_XATA_API_KEY: process.env.XATA_API_KEY,
-      NEXT_PUBLIC_XATA_BRANCH: process.env.XATA_BRANCH 
+      XATA_API_KEY: process.env.XATA_API_KEY,
+      XATA_BRANCH: process.env.XATA_BRANCH 
     },
   };
   
