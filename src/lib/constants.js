db4eec3147f3239cf40e0b2d0349e846ea28339c
nosmeri <parkdohyeon622@gmail.com>
2026-01-06 08:42:43 +0900

    chore: add premium member names.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -7,7 +7,7 @@ export const STUDY_PERIOD_OPTIONS = [
 export const DEFAULT_KAKAO_URL = "https://open.kakao.com/o/s66ruUkh"
 export const SUPPORT_CONTACT = "010-8602-9329"
 
-export const PREMIUM_MEMBER_NAMES = ["안채헌", "윤석영", "정준호"]
+export const PREMIUM_MEMBER_NAMES = ["안채헌", "윤석영", "정준호", "박도현"]
 
 export const DEFAULT_APPLICANT_OPTIONS = [2, 3, 4, 5]
 export const EXTENDED_APPLICANT_OPTIONS = Array.from({ length: 20 }, (_, i) => i + 1)
